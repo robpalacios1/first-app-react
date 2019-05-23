efe06f807a14e0b8991e9687374f1d3aaff62b5b
Roberto <robpalacios1@hotmail.com>
2019-05-23 01:08:22 -0500

    first app react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>Mi primera aplicación</h1>
+        <p>Esta es mi primera aplicación en React, está padrísimo!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
